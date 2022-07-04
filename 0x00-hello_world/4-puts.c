cfe1ba6578d3d52c5270aaa7fb341fb3194d6714
root <root@perezyeboah.localdomain>
2022-07-04 22:51:26 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+/**
+ *main is our entry point
+ *Return is zero always
+ */
 int main(void)
 {
-	char txt[] = "Programming is like building a multilingual puzzle";
+	char txt[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(txt);
 
-	return 0;
+	return (0);
 
 }
 
