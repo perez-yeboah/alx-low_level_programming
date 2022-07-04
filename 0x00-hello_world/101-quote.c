a98a066bacb2657b63fd8e96e0178c236482d960
root <root@perezyeboah.localdomain>
2022-07-04 23:27:38 +0000

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - This is our entry point
+ *
+ * Return: 1 error
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+
+}
+
