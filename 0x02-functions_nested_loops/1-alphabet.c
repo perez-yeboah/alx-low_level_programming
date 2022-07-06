6de9a00493386b5b018f12e0ea3f59fee627f26a
root <root@perezyeboah.localdomain>
2022-07-06 23:33:11 +0000

    updates

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
  */
 void print_alphabet(void)
 {
-	char line[] = "abcfefghijklmnopqrstuvwxyz";
+	char line[] = "abcdefghijklmnopqrstuvwxyz";
 
 	int i = 0;
 
@@ -15,7 +15,7 @@ void print_alphabet(void)
 
 	for (i = 0; i < size - 1; i++)
 	{
-	        _putchar(line[i]);
+		_putchar(line[i]);
 	}
 
 	_putchar('\n');
