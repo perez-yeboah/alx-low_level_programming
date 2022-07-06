4c7ae1dbb3f5e46650f110f974506471b6081273
root <root@perezyeboah.localdomain>
2022-07-06 23:39:45 +0000

    updates

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - print letters
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char line[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i = 0;
+
+	int j = 0;
+
+	int size = sizeof(line) / sizeof(line[0]);
+
+	for (j = 0; j < 10; j++)
+	{
+
+		for (i = 0; i < size - 1; i++)
+		{
+			_putchar(line[i]);
+		}
+
+		_putchar('\n');
+	}
+}
