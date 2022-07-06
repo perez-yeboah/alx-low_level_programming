70068280948a3f8cb2ac14c2da440a0931f00d28
root <root@perezyeboah.localdomain>
2022-07-06 23:53:40 +0000

    updates

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,11 @@
 
 /**
  * _islower - checks if letter is lowercase
+ * @c: Tje character to print
  *
  * Return : 1 if lower
  */
 int _islower(int c)
 {
-	return c >= 97 && c <= 122 ? 1 : 0;
+	return (c >= 97 && c <= 122 ? 1 : 0);
 }
