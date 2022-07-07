47dc6390e4b0d4abc8f9cadefd66df9a1d4a6785
root <root@perezyeboah.localdomain>
2022-07-07 01:52:14 +0000

    u

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,37 @@ void times_table(void)
 {
 	int i = 0, j = 0;
 
+	int t, o;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(48 + (i*j));
-			_putchar(',');
-			_putchar(' ');
+			int temp = i * j;
+
+			t = temp / 10;
+			o = temp % 10;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (temp < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(o + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(t + '0');
+				_putchar(o + '0');
+				
+			}
+
 		}
 		_putchar('\n');
 	}
