86fe39b273ec41bf95411ac5524c683d6236fb3e
root <root@perezyeboah.localdomain>
2022-07-07 01:25:07 +0000

    updates

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - print times table
+ *
+ */
+void times_table(void)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j=0; j < 10; j++)
+		{
+			_putchar(i*j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
