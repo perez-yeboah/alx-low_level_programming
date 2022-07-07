76b5bb2ca59441a581ae851ae8c323b79d5a396f
root <root@perezyeboah.localdomain>
2022-07-07 01:42:10 +0000

    aa

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j=0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar((char) (i*j));
+			_putchar(48 + (i*j));
 			_putchar(',');
 			_putchar(' ');
 		}
