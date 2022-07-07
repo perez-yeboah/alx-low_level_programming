c5cb9b2303047b33ab4b6b682088a0c748ff594d
root <root@perezyeboah.localdomain>
2022-07-07 00:30:36 +0000

    updates

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - checks if letter is lowercase
+ * @c: Tje character to print
+ *
+ * Return: 1 if lower
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
