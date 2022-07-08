ed996a6a77b43ebf93995c963ac06b9354d16daf
root <root@perezyeboah.localdomain>
2022-07-08 00:11:46 +0000

    updates

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,8 +6,8 @@
  * * left in the year, taking leap years into account
  * * @month: month in number format
  * * @day: day of month
- * * @year: year
- * * Return: void
+ * :* @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
