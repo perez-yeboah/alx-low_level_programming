323498fa7ed71527bc310115c53313b511fdd675
root <root@perezyeboah.localdomain>
2022-07-08 23:27:06 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is supper
+ * @c: character input
+ *
+ * Return: 1 if upper  0 if lower
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90 ? 1 : 0);
+}
+
