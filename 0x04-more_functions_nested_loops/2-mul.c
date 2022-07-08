789e64221486eff82ca3beeda6a5e427203aaee0
root <root@perezyeboah.localdomain>
2022-07-08 23:54:12 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - mul 2 numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ void _putchar(char);
 int _isupper(int);
 
 int _isdigit(int c);
+
+int mul(int, int);
