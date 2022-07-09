b38af4459d60a707d90de5178178cc441394b5a4
root <root@perezyeboah.localdomain>
2022-07-09 02:27:44 +0000

    updates

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 success 1 failure
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		bool mul3 = i % 3 == 0;
+		bool mul5 = i % 5 == 0;
+
+		if (mul3 & mul5)
+		{
+			printf("FizzBuzz");
+		}
+		else if (mul3)
+		{
+			printf("Fizz");
+		}
+		else if (mul5)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+
+
+
