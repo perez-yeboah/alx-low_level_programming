93d390cf2bea1c339bec2fc6bb7bfee908ea1657
root <root@perezyeboah.localdomain>
2022-07-09 01:10:27 +0000

    updates

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print
+ * @n: n char
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
