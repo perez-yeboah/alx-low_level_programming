3a3646657a4a32901804d28310a14d7901d54f65
root <root@perezyeboah.localdomain>
2022-07-09 02:50:31 +0000

    updates

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(long n)
+{
+	int count = 0;
+
+	long i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			count += 1;
+		}
+	}
+	return (count == 2 ? true : false);
+}
+
+int main(void)
+{
+	long num = 100;
+	long max = 0;
+
+	long i;
+
+	for (i = 1; i <= num; i++)
+	{
+		if (num % i == 0 && isPrime(i) && i > max)
+		{
+			max = i;
+		}
+	}
+	printf("%ld\n", max);
+
+	return (0);
+}
+
