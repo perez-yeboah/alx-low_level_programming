a1743a3be8b775242de4eacba68d1ec2266cc58f
root <root@perezyeboah.localdomain>
2022-07-29 00:10:46 +0000

    task 0 completed

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - memory allocation using malloc
+ * @b: int size of memory to be allocated
+ * Return: pointer to memory allocation
+ */
+void *malloc_cjecked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
